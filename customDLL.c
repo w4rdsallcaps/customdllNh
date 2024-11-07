1ce5ebf101ecefa3e9177bb0b5073226ba0eb745
DairuuSaito <140672617+DairuuSaito@users.noreply.github.com>
2024-11-07 22:01:29 +0800

    Add files via upload

---
+++ customDLL.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "customDLL.h"
+
+__declspec(dllexport) int intIn(int *target){
+    char buffer[999];
+    int correct = 1;
+    int value;
+
+    if (fgets(buffer, sizeof(buffer), stdin) != 0){
+        for (int i = 0; i < sizeof(buffer); i++){
+            if (buffer[i] == '\0' || buffer[i] == '\n'){
+                break;
+            }
+
+            if ((buffer[i] > '9' || buffer[i] < '0') && correct == 1){
+                fprintf(stderr, "Error: Char %c", buffer[i]);
+                correct = 0;
+            }
+        }
+
+        if (correct == 0) {return 0;}
+
+        sscanf(buffer, "%d", &value);
+        *target = value;
+
+        return 1;
+
+    }
+
+    return 0;
+}
---
+++ customDLL.h
@@ -0,0 +1,22 @@
+#ifndef customDLL
+#define customDLL
+
+#include <stdio.h>
+
+__declspec(dllexport) int intIn(int *target);
+
+#define LOG "LOG"
+#define ERROR "ERROR"
+#define DEBUG "DEBUG"
+
+#define typeOf(data, type) _Generic((data), type: 1, default: 0);
+
+#define errOut(type, message, other) _Generic((other), \
+    int:    fprintf(stderr, "%s: %s %d\n", type, message, other), \
+    char:   fprintf(stderr, "%s: %s %c\n", type, message, other), \
+    float:  fprintf(stderr, "%s: %s %f\n", type, message, other), \
+    double: fprintf(stderr, "%s: %s %f\n", type, message, other), \
+    default: fprintf(stderr, "%s: %s (unsupported type)\n", type, message) \
+);
+
+#endif
